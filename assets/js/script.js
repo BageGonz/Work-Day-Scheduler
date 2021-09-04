ba2dc3ba74f514c335c3f0196ddb2c007a38d48a
Gabriel Gonzalez <80490886+BageGonz@users.noreply.github.com>
2021-09-03 23:31:21 -0400

    fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,6 @@
 $(document).ready(function () {
     $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
 
-    $(".saveBtn").on("click", function () {
-        var text = $(this).siblings(".description").val();
-        var time = $(this).parent().attr("id");
-        localStorage.setItem(text, time);
-    });
-
-    $("#hour9 .description").val(localStorage.getItem("hour9"));
-    $("#hour10 .description").val(localStorage.getItem("hour10"));
-    $("#hour11 .description").val(localStorage.getItem("hour11"));
-    $("#hour12 .description").val(localStorage.getItem("hour12"));
-    $("#hour13 .description").val(localStorage.getItem("hour13"));
-    $("#hour14 .description").val(localStorage.getItem("hour14"));
-    $("#hour15 .description").val(localStorage.getItem("hour15"));
-    $("#hour16 .description").val(localStorage.getItem("hour16"));
-    $("#hour17 .description").val(localStorage.getItem("hour17"));
-
     function timeCheck() {
         var currentTime = moment().hours();
         $(".time-block").each(function () {
@@ -35,6 +19,15 @@ $(document).ready(function () {
             }
         })
     }
+    $(".saveBtn").on("click", function () {
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+        localStorage.setItem(text, time);
+    });
+
+    const timeArray = [$("#hour9"),$("#hour10"),$("#hour11"),$("#hour12"),$("#hour13"),$("#hour14"),$("#hour15"),$("#hour16"),$("#hour17"),];
+
+
 
     timeCheck();
 });
