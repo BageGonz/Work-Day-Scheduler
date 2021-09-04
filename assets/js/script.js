24c048b01c086902177b59c12a2961d6438b19e4
Gabriel Gonzalez <80490886+BageGonz@users.noreply.github.com>
2021-09-03 23:24:41 -0400

    added js feature to add and remove css from moment

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,45 @@
 $(document).ready(function () {
     $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
 
-    $(".saveBtn").on("click", function (){
+    $(".saveBtn").on("click", function () {
         var text = $(this).siblings(".description").val();
         var time = $(this).parent().attr("id");
-        events.push({ description: text, time:time});
-        localStorage.setItem("events", JSON.stringify(events));
-    })
-})
+        localStorage.setItem(text, time);
+    });
 
-$("#hour9 .description").val(localStorage.getItem("hour9"));
-$("#hour10 .description").val(localStorage.getItem("hour10"));
-$("#hour11 .description").val(localStorage.getItem("hour11"));
-$("#hour12 .description").val(localStorage.getItem("hour12"));
-$("#hour13 .description").val(localStorage.getItem("hour13"));
-$("#hour14 .description").val(localStorage.getItem("hour14"));
-$("#hour15 .description").val(localStorage.getItem("hour15"));
-$("#hour16 .description").val(localStorage.getItem("hour16"));
-$("#hour17 .description").val(localStorage.getItem("hour17"));
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
+
+    function timeCheck() {
+        var currentTime = moment().hours();
+        $(".time-block").each(function () {
+            var taskBlock = parseInt($(this).attr("id").split("hour")[1]);
+            if (taskBlock > currentTime) {
+                $(this).classList.add("past");
+            }
+            else if (taskBlock === currentTime) {
+                $(this).addClass("present");
+                $(this).removeClass("past");
+            }
+            else {
+                $(this).addClass("future");
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+            }
+        })
+    }
+
+    timeCheck();
+});
+
+
 
 
 
